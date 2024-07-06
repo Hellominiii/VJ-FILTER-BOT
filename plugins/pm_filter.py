94cfd6bc2b8625b78562a3d5cc5973ba3c06ef09
Hellominiii <137507094+Hellominiii@users.noreply.github.com>
2024-07-06 19:49:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -293,15 +293,12 @@ async def advantage_spoll_choker(bot, query):
             else:
                 reqstr1 = query.from_user.id if query.from_user else 0
                 reqstr = await bot.get_users(reqstr1)
-                btn.insert(0, [
-            InlineKeyboardButton("‼️ Hᴏᴡ Tᴏ Dᴏᴡɴʟᴏᴀᴅ Mᴏᴠɪᴇ ‼️", url=f'https://t.me/how_to_download_watch_88')
-        ])
                 if NO_RESULTS_MSG:
                     await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
                 k = await query.message.edit(script.MVE_NT_FND)
-                button = [[
-            InlineKeyboardButton("Gᴏᴏɢʟᴇ", url=f"https://www.google.com/search?q={reqst_gle}")
-        ]]
+                 btn.insert(0, [
+            InlineKeyboardButton("‼️ Hᴏᴡ Tᴏ Dᴏᴡɴʟᴏᴀᴅ Mᴏᴠɪᴇ ‼️", url=f'https://t.me/how_to_download_watch_88')
+        ])
                 await asyncio.sleep(10)
                 await k.delete()
 
